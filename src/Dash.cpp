653c92bba012e97636a0ad999c52000b7aa219c8
Tobias Bielohlawek <tobias.bielohlawek@solarisbank.de>
2018-01-15 09:23:12 +0100

    Add LED timer

--- src/Dash.cpp
+++ src/Dash.cpp
@@ -3,10 +3,10 @@
 
 LED DashClass::leds[] =
 {
-  {25, 24, 19, 0}, //1 { PE_4, PE_3, PC_4, 0 },
-  { 0,  2,  6, 0}, //3 { PA_6, PA_5, PC_4, 0 } ,
-  {22, 21,  1, 0}, //2 { PE_1, PE_0, PA_7, 0 },
-  {12, 11, 13, 0}  //4 { PC_3, PC_2, PC_1, 0 },
+  {25, 24, 19, WHITE, FULL}, //1 { PE_4, PE_3, PC_4, 0 },
+  { 0,  2,  6, WHITE, FULL}, //3 { PA_6, PA_5, PC_4, 0 } ,
+  {22, 21,  1, WHITE, FULL}, //2 { PE_1, PE_0, PA_7, 0 },
+  {12, 11, 13, WHITE, FULL}  //4 { PC_3, PC_2, PC_1, 0 },
 };
 
 KEY DashClass::keys[] =
@@ -17,19 +17,27 @@ KEY DashClass::keys[] =
   {20, 0}
 };
 
-void timer_handler(uint32_t data) {
+void dash_key_handler(uint32_t data) {
   uint8_t key_num;
   for (key_num = 0; key_num < 4; key_num++) {
     uint32_t state = Dash.key(key_num);
-    if (key_num == 1 && state == 50) {
+    if (key_num == K2 && state == 50) {
       Dash.led(key_num, RED);
       sys_reset();
-    } else if (key_num == 4 && state == 50) {
+    } else if (key_num == K4 && state == 50) {
       Dash.led(key_num, RED);
       Dash.off();
     } else {
       Dash.key_handler(key_num, state);
     }
+  }
+}
+
+void dash_led_handler(uint32_t data) {
+  uint8_t led_num;
+  Dash.next_frame();
+  for (led_num = 0; led_num < 4; led_num++) {
+    Dash.frame_led(led_num);
   }
 }
 
@@ -45,9 +53,8 @@ void DashClass::key_init(uint8_t key_num)
   pinMode(keys[key_num].pin, INPUT_PULLUP);
 }
 
-void DashClass::begin(void (*handler)(uint8_t, int32_t))
+void DashClass::begin()
 {
-  key_handler = handler;
   uint8_t led_num;
   for (led_num = 0; led_num < 4; led_num++) {
     printf("led %d init:\r\n", led_num);
@@ -56,24 +63,32 @@ void DashClass::begin(void (*handler)(uint8_t, int32_t))
     printf("key %d init:\r\n", led_num);
     key_init(led_num);
   }
-
-  GTimer.begin(0, 1 * 1000 * 50, timer_handler);
+  GTimer.begin(1, 5, dash_led_handler);
 }
 
-void DashClass::led(uint8_t led_num, uint8_t rgb)
+void DashClass::begin_key(void (*handler)(uint8_t, int32_t))
+{
+  key_handler = handler;
+  GTimer.begin(0, 1 * 1000 * 50, dash_key_handler);
+}
+
+void DashClass::stop()
+{
+  GTimer.stop(0);
+  GTimer.stop(1);
+}
+
+void DashClass::led(uint8_t led_num, uint8_t rgb, uint8_t brightness)
 {
   leds[led_num].state = rgb;
+  leds[led_num].brightness = brightness;
+}
+
+void DashClass::raw_led(uint8_t led_num, uint8_t rgb)
+{
   digitalWrite(leds[led_num].r, (~rgb >> 2) & 1);
   digitalWrite(leds[led_num].g, (~rgb >> 1) & 1);
   digitalWrite(leds[led_num].b, (~rgb >> 0) & 1);
-}
-
-void DashClass::all_led(uint8_t rgb)
-{
-  byte led_num;
-  for (led_num = 0; led_num < 4; led_num++) {
-    led(led_num, rgb);
-  }
 }
 
 int32_t DashClass::key(uint8_t key_num)
@@ -107,6 +122,19 @@ uint32_t DashClass::battery()
 void DashClass::off()
 {
   pinMode(PIN_PWR, OUTPUT);
+}
+
+void DashClass::frame_led(uint8_t led_num)
+{
+  raw_led(led_num, (dash_frame < leds[led_num].brightness) ? leds[led_num].state : OFF);
+}
+
+void DashClass::next_frame()
+{
+  dash_frame++;
+  if (dash_frame > FULL) {
+    dash_frame = 0;
+  }
 }
 
 DashClass Dash;
--- src/Dash.h
+++ src/Dash.h
@@ -5,6 +5,7 @@
 #include <sys_api.h>
 
 #define OFF    B000
+#define BLACK  B000
 #define RED    B100
 #define GREEN  B010
 #define YELLOW B110
@@ -13,12 +14,26 @@
 #define CYAN   B011
 #define WHITE  B111
 
+#define L1 0
+#define L2 1
+#define L3 2
+#define L4 3
+
+#define K1 0
+#define K2 1
+#define K3 2
+#define K4 3
+
+
+#define FULL 8
+
 typedef struct
 {
   uint8_t r;
   uint8_t g;
   uint8_t b;
   uint8_t state;
+  uint8_t brightness;
 } LED;
 
 typedef struct
@@ -42,18 +57,28 @@ class DashClass
     //  pinMode(led1_g, OUTPUT);
     //  pinMode(led1_r, OUTPUT);
     //  pinMode(key1, INPUT_PULLUP);
-    void begin(void (*handler)(uint8_t, int32_t));
-    void led(uint8_t led_num, uint8_t rgb);
-    void all_led(uint8_t rgb);
+    void begin();
+    void begin_key(void (*handler)(uint8_t, int32_t));
+    void stop();
+
+    void led(uint8_t led_num, uint8_t rgb, uint8_t brightness = FULL);
     int32_t key(uint8_t key_num);
+
+    void raw_led(uint8_t led_num, uint8_t rgb);
     bool raw_key(uint8_t key_num);
+
     uint32_t battery();
     void off();
 
   private:
+    uint8_t dash_frame = 0;
+
     void led_init(uint8_t led_num);
     void key_init(uint8_t key_num);
-    friend void timer_handler(uint32_t data);
+    friend void dash_key_handler(uint32_t data);
+    friend void dash_led_handler(uint32_t data);
+    void frame_led(uint8_t led_num);
+    void next_frame();
 };
 
 extern DashClass Dash;
